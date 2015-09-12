747def50a12be1e4250dfdd8fc7bd0ff07c9224f
unknown <marial.gutierrez001@mymdc.net>
2015-09-12 00:23:17 -0400

    pset

--- psets/6.js
+++ psets/6.js
@@ -12,6 +12,19 @@ For example:
 Look up the JavaScript string reference to find methods which may be useful!
 */
 
-function mixUp(a, b) {
+function mixUp(word_a, word_b) {
   // write your solution here...
+var first_two = word_a.substring(0,2);
+var first_rest= word_a.substring(2, word_a.length);
+var second_two = word_b.substring(0,2);
+var second_rest = word_b.substring(2, word_b.length);
+var word2= second_two+first_rest;
+var word1 = first_two+second_rest;
+var final = word1 +" "+ word2;
+return final;
+
 }
+var word_a = prompt("Please provide a word");
+var word_b = prompt("please provide another word");
+var mix = mixUp(word_a, word_b);
+console.log(mix)
